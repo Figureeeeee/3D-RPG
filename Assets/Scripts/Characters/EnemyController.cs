8e4a8e3a942acb2fbf515209f5754057257c91d4
Figureeeeee <1157552798@qq.com>
2022-11-25 17:52:26 +0800

    Enemy Animator

--- Assets/Scripts/Characters/EnemyController.cs
+++ Assets/Scripts/Characters/EnemyController.cs
@@ -12,20 +12,37 @@ public class EnemyController : MonoBehaviour
 {
     private EnemyStates enemyStates;
     private NavMeshAgent agent;
+    private Animator anim;
 
     [Header("Basic Settings")]
     public float sightRadius;
+    public bool isGuard;
+    private float speed;
+    private GameObject attackTarget;
 
-
+    // bool配合动画
+    bool isWalk;
+    bool isChase;
+    bool isFollow;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        speed = agent.speed;
     }
 
     private void Update()
     {
         SwitchStates();
+        SwitchAnimation();
+    }
+
+    void SwitchAnimation()
+    {
+        anim.SetBool("Walk", isWalk);
+        anim.SetBool("Chase", isChase);
+        anim.SetBool("Follow", isFollow);
     }
 
     void SwitchStates()
@@ -34,7 +51,6 @@ public class EnemyController : MonoBehaviour
         if(FoundPlayer())
         {
             enemyStates = EnemyStates.CHASE;
-            Debug.Log("找到player");
         }
 
         switch(enemyStates)
@@ -44,6 +60,26 @@ public class EnemyController : MonoBehaviour
             case EnemyStates.PATROL:
                 break;
             case EnemyStates.CHASE:
+                //TODO:追Player
+
+                //TODO:在攻击范围内则攻击
+                //TODO:配合动画
+                isWalk = false;
+                isChase = true;
+
+                agent.speed = speed;
+                if(!FoundPlayer())
+                {
+                    //TODO:拉脱回到上一个状态
+                    isFollow = false;
+                    isChase = false;
+                    agent.destination = transform.position;
+                }
+                else
+                {
+                    isFollow = true;
+                    agent.destination = attackTarget.transform.position;
+                }
                 break;
             case EnemyStates.DEAD:
                 break;
@@ -57,9 +93,11 @@ public class EnemyController : MonoBehaviour
         {
             if(target.CompareTag("Player"))
             {
+                attackTarget = target.gameObject;
                 return true;
             }
         }
+        attackTarget = null;
         return false;
     }
 }
